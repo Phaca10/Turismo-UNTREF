1329fc980cfd817ed7214d7bd1828a26a01f9b90
TomiCantero99 <tommi.cantero99@gmail.com>
2024-12-02 11:09:26 -0300

    Se agrego funcionalidad de buscar contenido en enlace turismo

--- src/pages/turismo.jsx
+++ src/pages/turismo.jsx
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
+import Destinos from "../components/Destinos";
 
-export class turismo extends Component {
-  render() {
+const turismo = () => {
+    const [nombreQuery, setNombreQuery] = useState("");
+    const [categoriaQuery, setCategoriaQuery] = useState("");
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        setNombreQuery(event.target[0].value);
+        setCategoriaQuery(event.target[1].value);
+    }
+
     return (
-      <div>turismo</div>
+        <div>
+            <form action="" onSubmit={handleSearch}>
+ <input type="text" name='nombre' placeholder='museo,hotel,balneario..' style={{borderColor:"black", border:"solid", textAlign: "center", fontSize: "1rem", marginTop: "4rem", marginBottom: "4rem", fontWeight: "bold"}} />
+        <input type="text" name='categoria' placeholder='paseos,hospedaje...' style={{borderColor:"black", border:"solid", textAlign: "center", fontSize: "1rem", marginTop: "4rem", marginBottom: "4rem", fontWeight: "bold"}} /> 
+             <button type="submit"> Busqueda </button>
+
+            </form>
+            <Destinos nombre={nombreQuery} categoria={categoriaQuery} />
+        </div>
     )
-  }
 }
 
-export default turismo
+export default turismo;
