61e967fa7ecf245fad347394e15e1e26d3c556e7
Kevin Bustos <cowbustos@gmail.com>
2024-12-11 17:44:55 -0300

    revisando estado

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ImageCarousel from './components/Carousel';
-import './components/Navbar.css';
-import './components/Footer.css';
-import './components/Carousel.css';
 import './App.css';
 
 function App() {
