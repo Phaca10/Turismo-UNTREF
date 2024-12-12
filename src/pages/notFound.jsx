b0b054db5634d53697b157760e94b55c1258b76e
hbuzzi74 <hbuzzi@gmail.com>
2024-12-12 20:02:45 -0300

    Se corrigió el regreso a la home cuando hay error 404

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <>
-    <div style={{textAlign: "center", fontSize: "3rem", marginTop: "4rem", fontWeight: "bold"}}> 404 - Pagina no encontrada</div>
-    <Link to="/inicio" style={{textAlign: "center", fontSize: "1rem", marginTop: "4rem", color: "#696969", display: "block"}}>Volver a inicio</Link>
+      <div
+        style={{
+          textAlign: "center",
+          fontSize: "3rem",
+          marginTop: "4rem",
+          fontWeight: "bold",
+        }}
+      >
+        {" "}
+        404 - Pagina no encontrada
+      </div>
+      <Link
+        to="/"
+        style={{
+          textAlign: "center",
+          fontSize: "1rem",
+          marginTop: "4rem",
+          color: "#696969",
+          display: "block",
+        }}
+      >
+        Volver a inicio
+      </Link>
     </>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
