734bf628d39ad684efe1156cf8aae9d28df43ab3
TomiCantero99 <tommi.cantero99@gmail.com>
2024-12-12 21:30:13 -0300

    Agregando contenido a sección historia

--- src/pages/historia.jsx
+++ src/pages/historia.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function historia() {
   return (
-    <div>historia</div>
+    <div className='text-center text-white bg-sky-950'>
+        <img src="images/lamadridfondo.jpg" alt="images" className="h-96 w-full object-center-top"/>
+      <h1 className="text-4xl font-['Open_Sans'] p-5 font-bold" > Historia de General La Madrid </h1>
+
+              <p className="mb-4 m-4">
+                General La Madrid, fundada el 4 de octubre de 1889, es una ciudad ubicada en el sudoeste de la provincia de Buenos Aires, Argentina. Lleva el nombre del general Gregorio Aráoz de Lamadrid, un prócer de la independencia argentina.
+                Originalmente parte de la "Conquista del Desierto", la región fue colonizada por inmigrantes europeos, principalmente españoles e italianos, que se dedicaron a la agricultura y la ganadería. Estas actividades siguen siendo pilares importantes de la economía local.
+                A lo largo de los años, General La Madrid ha crecido de manera sostenida, manteniendo su carácter rural y sus tradiciones. La ciudad ha sido testigo de importantes cambios tecnológicos en la agricultura, adaptándose a las nuevas técnicas de cultivo y ganadería.
+                Un hito importante en la historia reciente de la ciudad fue la construcción del balneario municipal en la década de 1980. Este proyecto transformó un área antes subutilizada en un centro de recreación y turismo, convirtiéndose rápidamente en el corazón de la vida social de la comunidad.
+              </p>
+              <p className="mb-4 m-4">
+                Hoy, General La Madrid es conocida por su calidad de vida, su producción agropecuaria y, por supuesto, su hermoso balneario, que atrae a visitantes de toda la región, especialmente durante los calurosos meses de verano.
+              </p>
+    </div>
   )
 }
 
